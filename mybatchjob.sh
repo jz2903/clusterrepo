6f338f6399236e3616019dc8ca5e8b0326596299
Junhui <junhui@dhcp-10-29-62-129.dyn.mit.edu>
2022-01-04 17:14:17 -0500

    change email

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -6,7 +6,7 @@
 #SBATCH -o /home/username/clusterrepo/myoutputfile.out
 #SBATCH -e /home/username/clusterrepo/myerrorfile.err
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=username@mit.edu
+#SBATCH --mail-user=junhuiz@mit.edu
 
 module load julia/1.5.2
 
